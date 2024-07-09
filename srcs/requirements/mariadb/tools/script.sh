9cccec14b71718124723f97beb2c83481f3499da
Nico <nicolike3.0@gmail.com>
2024-07-09 21:13:09 +0200

    testing again

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
-mysql_install_db
-mysqld
+service mysql start;
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${WP_NAME}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${WP_USER}\`@'localhost' IDENTIFIED BY '${WP_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${WP_DATABASE}\`.* TO \`${WP_USER}\`@'%' IDENTIFIED BY '${WP_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${WP_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
+mysqladmin -u root -p$WP_PASSWORD shutdown
+exec mysqld_safe
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#mysql_install_db
+#mysqld
 
 
 # SI LO OTRO FUNCIONA, BORRAR ESTO
