fa0a1670ca43b2c7dca1beaede39d9b95e71f494
Nicolas maria Ortolan soto <nortolan@c1r3s3.42madrid.com>
2024-07-10 18:55:59 +0200

    added a comment and changed a credential to a variable

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -13,7 +13,7 @@ chmod +x wp-cli.phar
 if [ ! -f wp-config.php ]; then
     echo 'WordPress not found. Installing...'
     ./wp-cli.phar core download --allow-root
-    ./wp-cli.phar config create --dbname="$WP_DB_NAME" --dbuser="$WP_USER" --dbpass="$WP_PASSWORD" --dbhost=mariadb --allow-root
+    ./wp-cli.phar config create --dbname="$WP_DB_NAME" --dbuser="$WP_USER" --dbpass="$WP_PASSWORD" --dbhost="$MYSQL_HOST" --allow-root
     ./wp-cli.phar core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_NAME" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --skip-email --allow-root
     ./wp-cli.phar user create "$WP_USER_NAME" "$WP_USER_EMAIL" --role=author --user_pass="$WP_USER_PASSWORD" --allow-root
 else
