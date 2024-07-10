9707ba96fcc6e1526011b82d5fc5c74fece3a99a
Nicolas maria Ortolan soto <nortolan@c1r3s3.42madrid.com>
2024-07-10 17:35:33 +0200

    trying to make wordpress wait for mariadb

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#while ! mariadb -h$MYSQL_HOST -u$WP_DB_USER -p$WP_PASSWORD $WP_NAME --silent; do
-#	echo "[INFO] waiting for database..."
-#	sleep 1;
-#done
+while ! mariadb -h$MYSQL_HOST -u$WP_DB_USER -p$WP_PASSWORD $WP_NAME --silent; do
+	echo "[INFO] waiting for database..."
+	sleep 1;
+done
 
 cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
