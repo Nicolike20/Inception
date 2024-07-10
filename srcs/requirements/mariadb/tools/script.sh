438aa45fd4b92f2e2425a65c842c495f81b29f10
Nicolas maria Ortolan soto <nortolan@c1r3s3.42madrid.com>
2024-07-10 18:29:13 +0200

    it worked, trying something else now

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -28,10 +28,10 @@ DB_EXISTS=$(mysql -u root -p${WP_PASSWORD} -e "SHOW DATABASES LIKE '${WP_DB_NAME
 if [ $DB_EXISTS -eq 1 ]; then
     echo "Database ${WP_DB_NAME} does not exist. Creating..."
     # Initialize the database and user
-    mysql -u root -p${WP_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS \`${WP_DB_NAME}\`;"
-    mysql -u root -p${WP_PASSWORD} -e "CREATE USER IF NOT EXISTS \`${WP_USER}\`@'localhost' IDENTIFIED BY '${WP_PASSWORD}';"
-    mysql -u root -p${WP_PASSWORD} -e "GRANT ALL PRIVILEGES ON \`${WP_DB_NAME}\`.* TO \`${WP_USER}\`@'%' IDENTIFIED BY '${WP_PASSWORD}';"
-    mysql -u root -p${WP_PASSWORD} -e "FLUSH PRIVILEGES;"
+    mysql -e "CREATE DATABASE IF NOT EXISTS \`${WP_DB_NAME}\`;"
+    mysql -e "CREATE USER IF NOT EXISTS \`${WP_USER}\`@'localhost' IDENTIFIED BY '${WP_PASSWORD}';"
+    mysql -e "GRANT ALL PRIVILEGES ON \`${WP_DB_NAME}\`.* TO \`${WP_USER}\`@'%' IDENTIFIED BY '${WP_PASSWORD}';"
+    mysql -e "FLUSH PRIVILEGES;"
 else
     echo "Database ${WP_DB_NAME} already exists. Skipping initialization."
 fi
